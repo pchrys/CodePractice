cf46db8f33f74ed5990032fb7d98ea6f84a28f46
shaobo Liu <shaobo.liu@navico.com>
2018-02-14 10:00:48 -0600

    Fixed bugs in the code

---
+++ compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+
+  echo " Usage: $0 <file_name>"
+  exit 1
+fi
+
+filename=$1
+base=$(echo $filename | sed -e 's/\..\+//g')
+
+echo $base
+target=${base}
+
+cmd="g++ --std=c++11 -g ${filename} -o ${target}"
+
+echo "cmd: $cmd"
+
+eval $cmd
