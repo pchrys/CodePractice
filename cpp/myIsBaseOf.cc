3f54e161d9255e5a5b3a3bb66d3774e698170ade
shaobo Liu <shaobo.liu@navico.com>
2018-05-17 14:52:03 -0500

    update code for template function

--- cpp/myIsBaseOf.cc
+++ cpp/myIsBaseOf.cc
@@ -18,6 +18,24 @@ struct IsDerivedFrom
     static const int value = sizeof(test(static_cast<D*>(0))) == sizeof(yes);
 };
 
+template <typename B, typename D>
+struct IsTheSame
+{
+    class yes
+    {
+        int a[2];
+    };
+
+    class no
+    {
+    };
+
+    static no test(const B*);
+    static yes test( D*);
+
+    static const int value = sizeof(test(static_cast<B*>(0))) == sizeof(yes);
+};
+
 
 class A
 {
@@ -32,8 +50,13 @@ class C
 
 int main()
 {
-    int flag = IsDerivedFrom<A, >::value;
+    int flag = IsDerivedFrom<A, B>::value;
 
+    printf("flag = %d \n", flag);
+
+    flag = IsTheSame<A, A>::value;
+    printf("flag = %d \n", flag);
+    flag = IsTheSame<A, B>::value;
     printf("flag = %d \n", flag);
 
     return 0;
